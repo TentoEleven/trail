89b95c2a9953912e9a0e5a0c80c40bcb213772b9
TentoEleven <166827391+TentoEleven@users.noreply.github.com>
2024-04-12 20:00:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
-!pip install torch-lr-finder
+
 
 
 from albumentations.pytorch import ToTensorV2
